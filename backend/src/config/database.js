ed17de01204454e551dd65a94dd6bb3eda561f76
victorblevi <victorbatistalevi@gmail.com>
2018-07-08 19:22:06 -0300

    commit 2

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,5 +2,5 @@ const mongoose = require('mongoose')
 
 mongoose.Promise = Promise
 
-module.exports = mongoose.connect('mongodb://localhost/produto')
+module.exports = mongoose.connect('mongodb://ds045970.mlab.com:45970/heroku_tg2kvftb')
 
